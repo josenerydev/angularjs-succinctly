ac39a8f3c8d21efa18422fda5cec6e01e4810447
José Nery <josenerydev@gmail.com>
2020-07-31 16:10:43 -0300

    Rendering an HTML Snippet in a Directive

---
+++ 03-directives/03-html-snippet-directive/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('MyApp', []);
+
+app.directive('myWidget', function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    // template: `<p>Hello World</p>`,
+    templateUrl: 'widget.html',
+  };
+});
