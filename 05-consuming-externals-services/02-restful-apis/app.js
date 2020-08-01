a9a491bf839acf63d213beea64212e0a1908524e
José Nery <josenerydev@gmail.com>
2020-08-01 11:36:52 -0300

    Consuming RESTful APIs

---
+++ 05-consuming-externals-services/02-restful-apis/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('MyApp', ['ngResource']);
+
+app.factory('Post', function ($resource) {
+  return $resource('/api/posts/:id');
+});
+
+app.controller('PostIndexCtrl', function ($scope, Post) {
+  Post.query(function (data) {
+    $scope.posts = data;
+  });
+});
+
+app.controller('PostShowCtrl', function ($scope, Post) {
+  Post.get({ id: 1 }, function (data) {
+    $scope.post = data;
+  });
+});
