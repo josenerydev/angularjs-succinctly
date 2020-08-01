db0094afec8e951749291a79364d82efba95628c
José Nery <josenerydev@gmail.com>
2020-08-01 11:54:22 -0300

    Deferred and Promise

--- 05-consuming-externals-services/02-restful-apis/app.js
+++ 05-consuming-externals-services/02-restful-apis/app.js
@@ -1,7 +1,14 @@
 var app = angular.module('MyApp', ['ngResource']);
 
 app.factory('Post', function ($resource) {
-  return $resource('/api/posts/:id');
+  // return $resource('/api/posts/:id');
+  return $resource(
+    '/api/posts/:id',
+    {},
+    {
+      query: { method: 'GET', isArray: false },
+    },
+  );
 });
 
 app.controller('PostIndexCtrl', function ($scope, Post) {
@@ -15,3 +22,19 @@ app.controller('PostShowCtrl', function ($scope, Post) {
     $scope.post = data;
   });
 });
+
+var first = $http.get('/app/data/first.json'),
+  second = $http.get('/app/data/second.json'),
+  third = $http.get('/app/data/third.json');
+
+$q.all([first, second, third])
+  .then(function (result) {
+    var tmp = [];
+    angular.forEach(result, function (response) {
+      tmp.push(response.data);
+    });
+    return tmp;
+  })
+  .then(function (tmpResult) {
+    $scope.combinedResult = tmpResult.join(', ');
+  });
